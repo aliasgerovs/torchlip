a097d615da4fe707850774b5102e07808064314f
Ali Asgarov <alia.35@insightlynx.ai>
2023-12-11 17:09:04 -0500

    Updated

--- config.py
+++ config.py
@@ -1,46 +1,36 @@
-import gc
-import numpy as np
-import pandas as pd
-import os
+import torch
+
 class CFG:
-    # Basic configuration
     debug = False
     batch_size = 32
     num_workers = 2
     epochs = 5
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    # Paths
     image_path = image_path
     captions_path = captions_path
 
-    # Learning rates
     head_lr = 1e-3
     image_encoder_lr = 1e-4
     text_encoder_lr = 1e-5
 
-    # Model configuration
     model_name = 'resnet50'
     text_encoder_model = "distilbert-base-uncased"
     text_tokenizer = "distilbert-base-uncased"
 
-    # Embedding and projection dimensions
     image_embedding = 2048
     text_embedding = 768
     num_projection_layers = 1
     projection_dim = 256
 
-    # Training parameters
     weight_decay = 1e-3
     patience = 1
     factor = 0.7
     temperature = 1.0
     max_length = 200
 
-    # Pretraining and trainability
     pretrained = True
     trainable = True
 
-    # Image processing
-    size = 380  # image size
+    size = 380
     dropout = 0.1
---
+++ transoforms.py
@@ -0,0 +1,22 @@
+def get_transforms(mode="train"):
+    if mode == "train":
+        # In 'train' mode, we apply both resizing and a variety of data augmentation techniques.
+        return A.Compose([
+            A.Resize(CFG.size, CFG.size, always_apply=True),
+            A.HorizontalFlip(p=0.5),  # Randomly flip images horizontally
+            A.VerticalFlip(p=0.5),  # Randomly flip images vertically
+            A.RandomRotate90(p=0.5),  # Randomly rotate images by 90 degrees
+            A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.1, rotate_limit=45, p=0.5),  # Random shifts, scales, and rotations
+            A.Perspective(scale=(0.05, 0.1), p=0.5),  # Random perspective transformations
+            A.Blur(blur_limit=(3, 5), p=0.5),  # Apply Gaussian blur with a kernel size between 3 and 5
+            A.GaussNoise(var_limit=(10.0, 50.0), p=0.5),  # Add Gaussian noise to the images
+            A.Normalize(max_pixel_value=255.0, always_apply=True),
+            ToTensorV2(always_apply=True),  # Convert image to PyTorch tensor format
+        ])
+    else:
+        # In 'test' or other modes, we only apply the necessary resizing and normalization.
+        return A.Compose([
+            A.Resize(CFG.size, CFG.size, always_apply=True),
+            A.Normalize(max_pixel_value=255.0, always_apply=True),
+            ToTensorV2(always_apply=True),  # Convert image to PyTorch tensor format
+        ])
